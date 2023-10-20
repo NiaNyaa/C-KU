4a32af2ebbd4ae5e60eb2de61bc7031e3dffc496
NiaNyaa <109681800+NiaNyaa@users.noreply.github.com>
2023-10-21 01:22:47 +0700

    Create cs_variabel.cs

---
+++ cs_variabel.cs
@@ -0,0 +1,12 @@
+using System;
+
+class Program
+{
+    static void Main()
+    {
+        int nilai = 42;
+        Console.WriteLine(nilai);
+        Console.ReadLine();
+    }
+}
+// mirip c++ lah

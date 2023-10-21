1e59d244255aed862a128dbc085f932d7f42e2e9
NiaNyaa <109681800+NiaNyaa@users.noreply.github.com>
2023-10-21 19:42:33 +0700

    Create cs_list.cs

---
+++ cs_list.cs
@@ -0,0 +1,19 @@
+using System;
+using System.Collections.Generic;
+
+class Program
+{
+    static void Main()
+    {
+        List<int> numbers = new List<int>();
+        numbers.Add(1);
+        numbers.Add(2);
+        numbers.Add(3);
+        number.Remove(2) //untuk menghapus
+
+        foreach (int number in numbers)
+        {
+            Console.WriteLine(number);
+        }
+    }
+}

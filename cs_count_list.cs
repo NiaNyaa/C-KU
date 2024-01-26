3c0172df0c42a6ecc0e7750124ad6fb811dbe887
NiaNyaa <109681800+NiaNyaa@users.noreply.github.com>
2024-01-26 17:56:27 +0700

    Create cs_count_list.cs

---
+++ cs_count_list.cs
@@ -0,0 +1,15 @@
+using System;
+using System.Collections.Generic;
+
+class Program
+{
+    static void Main()
+    {
+        List<string> Nama = new List<string>();
+        Nama.Add("Udin");
+        Nama.Add("Bapak");
+        Nama.Add("Ibuk");
+
+        Console.WriteLine(Nama.Count);
+    }
+}

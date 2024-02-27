73bbbc564a6daef577be8d9a5a74c7f9a8cfacc9
NiaNyaa <109681800+NiaNyaa@users.noreply.github.com>
2024-02-27 12:47:28 +0700

    Update cs_ImGuiNet.cs

--- cs_ImGuiNet.cs
+++ cs_ImGuiNet.cs
@@ -1,6 +1,8 @@
 using ImGuiNET;
 using ClickableTransparentOverlay;
 using System.Threading.Tasks;
+using System.Threading;
+using System;
 
 namespace Program
 {
@@ -8,32 +10,40 @@ namespace Program
     {
         private int sliderValue = 0;
         private string inputText = "";
+        private int inputText2 = 1;
+
         protected override void Render()
         {
             ImGui.Begin("ImGuiNet");
-            ImGui.Text("By Ikhsan");
+            ImGui.Text("By MIH");
+            // slide
             ImGui.SliderInt("Slider", ref sliderValue, 0, 10);
             Console.WriteLine(sliderValue.ToString());
+            // label
             ImGui.Text(sliderValue.ToString());
-            ImGui.InputText("Input Field", ref inputText, 100);
+            // input string
+            ImGui.InputText("Input", ref inputText, 100);
             ImGui.Text(inputText);
+            // input int?
+            ImGui.InputInt("", ref inputText2, 1);
+            ImGui.Text(inputText2.ToString());
             ImGui.End();
         }
 
-        public override async Task Run()
+        public static void Logic()
         {
-  
             while (true)
             {
-                await Task.Delay(1000);
+                continue;
             }
         }
 
-        public static void Main(string[] args)
+        public static void Main()
         {
-            Program program = new Program();
+            Program program = new();
             program.Start().Wait();
-            program.Run().Wait();
+            Thread T1 = new(Logic) { IsBackground = true };
+            T1.Start();
         }
     }
 }
